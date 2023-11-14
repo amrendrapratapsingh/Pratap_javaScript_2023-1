0fb651cd0cdf356127b6b9014b7d76596d46663d
Amrendra <amrendrapratapcoders@gmail.com>
2023-11-14 22:19:27 +0530

    code for variable declaration

---
+++ Variable/first.js
@@ -0,0 +1,15 @@
+const accountId = 44777123;
+let accountEmail = "pratap@google.com" ;
+var accountPassword = "123456";
+let accountState;
+    accountCity = "Varanasi";
+
+    // accountId =  445693;
+    console.log(accountId);
+
+    /*
+    prefer not use var
+because var  keyword issues in block scope and function function
+    */
+
+    console.table([accountId,accountEmail,accountCity,accountState]); 
